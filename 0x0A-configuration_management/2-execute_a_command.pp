f862c655e92e66a86a523787c1c8b72319e29354
fk2019 <fkmuiruri8@gmail.com>
2023-03-30 07:07:16 -0700

    Remove double quotes on string

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # execute pkill command on killmenow process
 exec {'kill killmenow':
-  command => "/usr/bin/pkill killmenow",
+  command => '/usr/bin/pkill killmenow',
   path    => ['/usr/bin/pkill', 'usr/sbin/pkill']
 }
