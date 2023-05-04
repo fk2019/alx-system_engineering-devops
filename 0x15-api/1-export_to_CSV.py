7e555101f28d62cdad3efb16060300f9e5dc1b6b
fk2019 <fkmuiruri8@gmail.com>
2023-05-04 12:16:39 +0300

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Use API for a given user id and export information of
+   the person's TODO list progress in a csv file
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    req = requests.get(f'https://jsonplaceholder.typicode.com/users?id={id}')
+    name = req.json()[0].get('name')
+    reqt = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={id}')
+    tasks = reqt.json()
+    data = []
+    for i in tasks:
+        data.append({'name': name, 'id': id, 'completed': i['completed'],
+                     'title': i['title']})
+    with open('USER_ID.csv', 'w', newline='') as csvfile:
+        fn = ['id', 'name', 'completed', 'title']
+        writer = csv.DictWriter(csvfile, fieldnames=fn, quoting=csv.QUOTE_ALL)
+        for d in data:
+            writer.writerow(d)
