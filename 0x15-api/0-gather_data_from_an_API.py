bf3c9a7f6dedcacd1f9973f4a67e50688a6f5cb9
fk2019 <fkmuiruri8@gmail.com>
2023-05-04 11:34:34 +0300

    Use get dict function

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,19 +9,17 @@ import sys
 if __name__ == "__main__":
     id = sys.argv[1]
     req = requests.get(f'https://jsonplaceholder.typicode.com/users?id={id}')
-    name = req.json()[0]['name']
-    reqt = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={id}')
+    name = req.json()[0].get('name')
+    reqt = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={id}')
     total = len(reqt.json())
     count = 0
     tasks = []
     for i in reqt.json():
-        for k in i.keys():
-            if k == 'completed':
-                if i[k] is True:
-                    count = count + 1
-                    tasks.append(i)
-    print(f'Eployee {name} is done with tasks ({count}/{total}):')
+        if i.get('completed') is True:
+            count = count + 1
+            tasks.append(i)
+    print(f'Employee {name} is done with tasks ({count}/{total}):')
     for i in tasks:
-        for k in i.keys():
-            if k == 'title':
-                print(f'\t {i[k]}')
+        if i.get('title'):
+            print(f'\t {i.get("title")}')
