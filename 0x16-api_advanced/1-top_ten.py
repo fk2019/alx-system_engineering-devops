f7e34c4b9d59ceeb1ae2267fe56683895d621a91
fk2019 <fk2019@gmail.com>
2023-05-15 18:54:30 +0300

    Add top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Query Reddit API and print the titles of the first
+   10 hot posts for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Query number of subcsribers of subreddit
+    """
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)' +
+               'AppleWebKit/537.36 (KHTML, like Gecko)' +
+               'Chrome/58.0.3029.110 Safari/537.3'}
+    resp = requests.get('https://www.reddit.com/r/{}/hot/.json'.
+                        format(subreddit), allow_redirects=False)
+    json = resp.json()
+    if json.get('data'):
+        titles = [t['data']['title'] for t in json.get('data').get('children')]
+        top_ten = titles[:10]
+        for title in top_ten:
+            print(title)
+    else:
+        print("None")
