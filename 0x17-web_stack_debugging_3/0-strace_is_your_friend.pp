03e40360165801277f161d2865a58f48c2b088fe
root <fkmuiruri8@gmail.com>
2023-05-16 08:29:44 +0000

    Fix wordpress

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# fix wordpress badd php extension
+# fix bad wordpress php extension
 exec { 'fix wordpress':
           command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
           path    => ['/bin/sed']
+}
